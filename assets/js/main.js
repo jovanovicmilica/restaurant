dc64ea779968e6564b847e4da7c76dca125dda2d
Jovanovic Milica <46057613+jovanovicmilica@users.noreply.github.com>
2021-11-28 20:49:00 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,12 +13,12 @@ $(window).scroll(function(e){
     }
 })
 
-var navName=['Home','Menu','Events','About','Reservation','Author']
-var navLink=['index.html','menu.html','events.html','about.html','reservation.html','author.html']
+var navName=['Home','Menu','About','Reservation','Author']
+var navLink=['index.html','menu.html','about.html','reservation.html','#']
 function getNavigation(){
     var ispis='<ul id="navLarge" class="m-0 px-3">'
     for(let i=0;i<navName.length;i++){
-        ispis+=`<li><a class="text-decoration-none" href="${navLink[i]}">${navName[i]}</a></li>`
+        ispis+=`<li><a id="${navName[i]}" class="text-decoration-none" href="${navLink[i]}">${navName[i]}</a></li>`
     }
     ispis+="</ul>"
 
@@ -26,12 +26,13 @@ function getNavigation(){
         ispis+=`<a href="#"><i class="text-light fs-3 fa fa-bars"></i></a>`
         ispis+='<ul id="navSmall" class="m-0 px-3">'
         for(let i=0;i<navName.length;i++){
-            ispis+=`<li><a class="text-decoration-none" href="${navLink[i]}">${navName[i]}</a></li>`
+            ispis+=`<li><a id="${navName[i]}" class="text-decoration-none" href="${navLink[i]}">${navName[i]}</a></li>`
         }
         ispis+="</ul>"
     ispis+=`</div>`
 
     document.getElementById("nav").innerHTML=ispis
+    $("#Author").click(getAuthor)
 
     var bar=$("#mobileNav> a")
     bar.click(function(e){
@@ -45,13 +46,49 @@ function getNavigation(){
         })
 }
 
+function getAuthor(e){
+    e.preventDefault()
+    var print=`
+    <a href="#" id="close"><i class="fa fa-times fs-3"></i></a>
+    <div class="py-5 heading">
+    <h2 class="fs-1">Author</h2>
+</div>
 
+
+<div class="ImageWithText">
+    <div class="authorImage">
+        <img src="assets/images/author.jpg" alt="Nikolina Nikolic" class="img-fluid">
+    </div>
+    <div>
+        <h2 class="mb-2">Nikolina Nikolic 90/19</h2>
+        <p>Hello, I'm Nikolina Nikolic 21 year old web-developer from Serbia. I am currently studying Internet technology at the "Visoka ICT" School in Belgrade. </p>
+        <p>If you want to know more about my work you should check my  <a href="https://nikolinanikolic00.netlify.app/" class="amber-text " target="blank_">portfolio</a>,
+            or about my life, you can find me on  <a href="https://www.instagram.com/" target="blank_" class="amber-text">instagram</a>, <a href="https://www.facebook.com/" target="blank_" class="amber-text">facebook</a> or <a href="https://twitter.com/twitter" class="amber-text">twitter</a>. 
+            If you are interesting to work with me  <a href="https://www.linkedin.com/feed/" target="blank_" class="amber-text">linkedIn</a> is the perfect social media or you can just send me an email<a href="about.html" class="amber-text"> nikolina.nikolic.90.19@ict.edu.rs</a>.
+        </p>
+        <p>And finally the most personal information you can get about me is my phone number<a href="about.html" class="amber-text"> +381 621367719</a>. Don't be shy to call.
+        </p>
+    </div>
+</div>`
+
+    $("#modal").html(print)
+    $("#modal").removeClass("d-none")
+    $("body").addClass("scroll")
+
+    $("#close").click(close)
+}
+
+function close(e){
+    e.preventDefault()
+    $("#modal").addClass("d-none")
+    $("body").removeClass("scroll")
+}
 
 let url=location.href;
 if(url.indexOf("index.html")!=-1){
-    getOffer()
     getCustomers()
     getGallery()
+    getEvents()
 }
 if(url.indexOf("menu.html")!=-1){
     getMealTypes()
@@ -64,33 +101,22 @@ if(url.indexOf("menu.html")!=-1){
 
     printMeal(breakfast,breakfastImage,breakfastPrice)
 }
-if(url.indexOf("events.html")!=-1){
-    getEvents()
-}
+
 if(url.indexOf("reservation.html")!=-1){
     fillDdList()
     $("#buttonMessage").click(sendMessage)
+    $("#name").blur(validateName)
+    $("#email").blur(validateEmail)
+    $("#phone").blur(validatePhone)
+    $("#date").blur(validateDate)
+    $("#ddGuests").change(validateDdl)
+    $("#message").blur(validateMessage)
 }
 if(url.indexOf("about.html")!=-1){
     getSlider()
+    $("#more").click(showMore)
 }
 
-function getOffer(){
-    var image=['services1.svg','services2.svg','services3.svg']
-    var heading=['Healthy Meal','Fast Food','Delicious Coffee']
-    var text=["Healthy Meals You'll Want to Make All the Time.",'High Quality Fast Food with Competitive Price.',"You don't have to go far for the best tasting coffee."]
-    var ispis=''
-    for(let i=0;i<heading.length;i++){
-        ispis+=`<div class="oneOffer text-center my-2">
-            <div><img class="img-fluid" src="assets/images/${image[i]}" alt="${heading[i]}"/></div>
-            <h3>${heading[i]}</h3>
-            <p>${text[i]}</p>
-        </div>`
-    }
-
-    document.getElementById("offer").innerHTML=ispis
-
-}
 
 function getCustomers(){
     var image=['user1.jpg','user2.jpg','user3.jpg','user3.webp','user4.webp']
@@ -271,6 +297,92 @@ function formValidation(input,regularExpression){
     return error
 }
 
+function blurValidate(input,regularExpression){
+    if(!regularExpression.test(input.value)){
+        input.classList.add("border-danger")
+        input.classList.remove("border-success")
+        input.parentElement.nextElementSibling.classList.remove('d-none')
+    }
+    else{
+        input.classList.add("border-success")
+        input.classList.remove("border-danger")
+        input.parentElement.nextElementSibling.classList.add('d-none')
+    }
+}
+
+var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+var regName=/^[A-ZŽĐŠĆČ][a-zžđšćč]{2,}(\s[A-ZŽĐŠĆČ][a-zžđšćč]{2,}){1,2}$/;
+var regPhone=/^06[012345679]\d{6,7}$/;
+
+function validateName(){
+    var name=document.getElementById("name")
+
+    blurValidate(name,regName)
+}
+function validatePhone(){
+    var name=document.getElementById("phone")
+
+    blurValidate(name,regPhone)
+}
+function validateEmail(){
+    var name=document.getElementById("email")
+
+    blurValidate(name,regEmail)
+}
+
+function validateDate(){
+    var date=document.getElementById("date")
+    var currentTime=Date.now()
+    var choosenTime=0
+    if(date.value==""){
+        date.parentElement.nextElementSibling.classList.remove('d-none')
+        date.classList.add("border-danger")
+        date.classList.remove("border-success")
+    }
+    else{
+        choosenTime=Date.parse(date.value)
+    }
+    if(choosenTime<=currentTime){
+        date.parentElement.nextElementSibling.classList.remove('d-none')
+        date.classList.add("border-danger")
+        date.classList.remove("border-success")
+    }
+    else{
+        date.parentElement.nextElementSibling.classList.add('d-none')
+        date.classList.remove("border-danger")
+        date.classList.add("border-success")
+    }
+}
+
+function validateMessage(){
+    var message=document.getElementById("message")
+    
+    if(message.value.split(" ").length<5){
+        message.parentElement.nextElementSibling.classList.remove('d-none')
+        message.classList.add("border-danger")
+        message.classList.remove("border-success")
+    }
+    else{
+        message.parentElement.nextElementSibling.classList.add('d-none')
+        message.classList.remove("border-danger")
+        message.classList.add("border-success")
+    }
+}
+
+function validateDdl(){
+    var guestsNumber=document.getElementById("ddGuests")
+    if(guestsNumber.value==0){
+        guestsNumber.parentElement.nextElementSibling.classList.remove('d-none')
+        guestsNumber.classList.add("border-danger")
+        guestsNumber.classList.remove("border-success")
+    }
+    else{
+        guestsNumber.parentElement.nextElementSibling.classList.add('d-none')
+        guestsNumber.classList.remove("border-danger")
+        guestsNumber.classList.add("border-success")
+    }
+
+}
 
 function sendMessage(){
     var name=document.getElementById("name")
@@ -280,9 +392,6 @@ function sendMessage(){
     var guestsNumber=document.getElementById("ddGuests")
     var phone=document.getElementById("phone")
     
-    var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    var regName=/^[A-ZŽĐŠĆČ][a-zžđšćč]{2,}(\s[A-ZŽĐŠĆČ][a-zžđšćč]{2,}){1,2}$/;
-    var regPhone=/^06[012345679]\d{6,7}$/;
 
     var error=0
 
@@ -346,4 +455,10 @@ function getSlider(){
         setTimeout(slider, 2000);
     };
     slider();
+}
+
+function showMore(e){
+    e.preventDefault()
+    $(".ImageWithText p").removeClass("d-none")
+    $("#more").hide()
 }
